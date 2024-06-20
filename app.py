9f5cc0c36a2a76fed1fa9e09f5c30eac2cbf24a4
Samuel Waweru <samuelgatoto81@gmail.com>
2024-06-20 12:34:50 +0300

    update streamlit ui dependency

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+# importing Langchain dependencies
 from langchain.document_loaders import PyPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+#Bring in Streamlit for UI dev
+import streamlit as st
+
+
+
